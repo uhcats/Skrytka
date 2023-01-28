6d27135e3e8a695f6f88d25dca81c650a13555eb
Stachu <stas1111pl@wp.pl>
2023-01-28 21:22:05 +0100

    Make route to quiz page and reafactoring truck pg

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quiz = () => {
+  return (
+    <div className="popup">
+      <button className='startQuizBtn'>Start</button>
+      <button className='backSiteQuizBtn'>Wróc do poprzedniej strony</button>
+    </div>
+  )
+}
+
+export default Quiz;
